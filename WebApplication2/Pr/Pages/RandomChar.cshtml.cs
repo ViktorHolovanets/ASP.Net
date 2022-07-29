f9662aad63b1f77389c353bc14d4048640d5e25f
ViktorHolovanets <victorgolova@gmail.com>
2022-07-29 20:35:55 +0300

    1

--- WebApplication2/Pr/Pages/RandomChar.cshtml.cs
+++ WebApplication2/Pr/Pages/RandomChar.cshtml.cs
@@ -8,10 +8,11 @@ namespace Pr.Pages
 {
     public class RandomCharModel : PageModel
     {
+        Random r=new Random();
         public char SymbolUnicode { get; set; }
         public void OnGet()
         {
-            SymbolUnicode = Convert.ToChar(new Random().Next(65,122));
+            SymbolUnicode = Convert.ToChar(r.Next(-1,1)==0? r.Next(65,90):r.Next(97, 122));
         }
     }
 }
