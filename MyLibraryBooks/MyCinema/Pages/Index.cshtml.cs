0bae1afa76e674ca2fe920423b19b444871f34de
ViktorHolovanets <victorgolova@gmail.com>
2022-08-08 16:01:21 +0300

    the end

--- MyLibraryBooks/MyCinema/Pages/Index.cshtml.cs
+++ MyLibraryBooks/MyCinema/Pages/Index.cshtml.cs
@@ -1,20 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Library;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace MyCinema.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
+        public LibraryDB context { get; private set; }
+        public IEnumerable<Schedule> ScheduleToday { get; set; } = null;
+        public IndexModel(LibraryDB db)
         {
-            _logger = logger;
+            context = db;
         }
 
         public void OnGet()
         {
+            string date = DateTime.Today.ToString("yyyy-MM-dd");
+            ScheduleToday = context.Schedules.Include(c => c.Film_id).Where(s => s.Date == date).ToList();
+        }
 
+        public IEnumerable<Schedule> GetFilm(Film film)
+        {
+            var t = ScheduleToday.Where(s => s.Film_id.Id == film.Id).ToList();
+            return t;
         }
     }
 }
--- MyLibraryBooks/MyCinema/wwwroot/js/site.js
+++ MyLibraryBooks/MyCinema/wwwroot/js/site.js
@@ -2,3 +2,24 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(() => {
+    $('.select-film-details').on('click',
+        function() {
+            let parent = $(this).closest('div');
+            $("#poster").attr("src", parent.find('.select_film_poster').val());
+            $("#titli").html(`${parent.find('.select_film_title').val()}`);
+            $("#released").html(`${parent.find('.select_film_released').val()}`);
+            $("#genre").html(`${parent.find('.select_film_genre').val()}`);
+            $("#country").html(`${parent.find('.select_film_country').val()}`);
+            $("#director").html(`${parent.find('.select_film_director').val()}`);
+            $("#writer").html(`${parent.find('.select_film_writer').val()}`);
+            $("#actors").html(`${parent.find('.select_film_actors').val()}`);
+            $("#award").html(`${parent.find('.select_film_awards').val()}`);
+            $('#modal-window').fadeIn();
+        });
+    $('#close').on('click',
+        function() {
+            $('#modal-window').fadeOut();
+            return false;
+        });
+})
