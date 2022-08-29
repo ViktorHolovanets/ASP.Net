c075490b433d0c07357e16e110e78fc0bf80b692
ViktorHolovanets <victorgolova@gmail.com>
2022-08-29 17:37:02 +0300

    tag helper

---
+++ WebApplication_MVC_Bind/Tag_Helper/Models/BankCard.cs
@@ -0,0 +1,12 @@
+﻿namespace Tag_Helper.Models
+{
+    public class BankCard
+    {
+        public string? Id { get; set; }
+        public string NumbCard { get; set; } = "";
+        public string SurnameClientBank { get; set; } = "";
+        public int Month { get; set; }
+        public int Year { get; set; }
+        public string CVN { get; set; }
+    }
+}
---
+++ WebApplication_MVC_Bind/Tag_Helper/TagHelpers/MymenuTagHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Tag_Helper.TagHelpers
+{
+    public class MymenuTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";        
+            output.Content.SetContent($"Текущее время: {DateTime.Now.ToString("HH:mm:ss")}");
+        }
+    }
+    public class TimerTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";    // заменяет тег <timer> тегом <div>
+            // устанавливаем содержимое элемента
+            output.Content.SetContent($"Текущее время: {DateTime.Now.ToString("HH:mm:ss")}");
+        }
+    }
+}
