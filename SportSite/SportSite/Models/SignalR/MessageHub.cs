456949b8ae0c6c69966acbee3e5f8257c1f04f1b
ViktorHolovanets <victorgolova@gmail.com>
2022-09-26 20:54:45 +0300

    1

--- SportSite/SportSite/Models/SignalR/MessageHub.cs
+++ SportSite/SportSite/Models/SignalR/MessageHub.cs
@@ -3,10 +3,5 @@
 namespace SportSite.Models.SignalR
 {
     public class MessageHub : Hub
-    {
-        //public async Task Send(MessageSignalR message)
-        //{
-        //    await this.Clients.All.SendAsync("Receive", message);
-        //}
-    }
+    { }
 }
